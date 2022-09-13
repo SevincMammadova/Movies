87d9e133edfc001ad4526dd6960e489b525b5202
SevincMammadova <sevincmammadova609@gmail.com>
2022-09-13 19:42:35 +0400

    MN-010, task done

---
+++ src/packages/utils/emailKey.ts
@@ -0,0 +1,6 @@
+/* eslint-disable import/no-default-export */
+export default {
+    USER_ID: 'tOZXbbuB2FpigH0Ox', //userID
+    TEMPLATE_ID: 'template_qbq9mtd',
+    SERVICE_ID: 'service_6hkhq47'
+};
